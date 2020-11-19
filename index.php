bf116eff329f77bd77e7a104edddaabcae8c0d54
ecw0006 <ecw0006@mix.wvu.edu>
2020-11-19 11:04:59 -0800

    about page fixes

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 <link rel="stylesheet" href="css/about.css">
     <div class="about" style="color: azure;">
         <h1>About Us</h1>
-        <p>dkflfa;grgraegare;oglflslfeaf</p>
+        <p>Meet Our Staff</p>
     </div>
     <div class="row">
         <div class="column">
             <div class="card">
-                <img src="images/dog.jpg" <div class="container">
+                <img src="images/dog.jpg"> <div class="container">
                 <h2>CEO</h2>
                 <p> Dog </p>
                 <p> Email: dogsrule@cs230.com</p>
@@ -22,7 +22,7 @@
     </div>
     <div class="column">
         <div class="card">
-            <img src="images/chewwy.jpg" <div class="container">
+            <img src="images/chewwy.jpg"> <div class="container">
             <h2>CTO</h2>
             <p> Chewbacca </p>
             <p> Email: chewwy@cs230.com</p>
@@ -32,7 +32,7 @@
     </div>
     <div class="column">
         <div class="card">
-            <img src="images/AntonioBrown.jpg" <div class="container">
+            <img src="images/AntonioBrown.jpg"> <div class="container">
             <h2>CMO</h2>
             <p> Antonio Brown </p>
             <p> Email: ab@cs230.com</p>
@@ -42,7 +42,7 @@
     </div>
     <div class="column">
         <div class="card">
-            <img src="images/cat.jpg" <div class="container">
+            <img src="images/cat.jpg"> <div class="container">
             <h2>Art director</h2>
             <p> Cat </p>
             <p> Email: cat@cs230.com</p>
